541e6b580c97f7ca35d98aa150c20a6cf0229c9e
Sgr A* VMT <1611902585@qq.com>
2026-01-28 09:50:09 +0000

    回退修复

--- scanner.py
+++ scanner.py
@@ -738,7 +738,6 @@ class Scanner:
                 axis_twist_comp = type("class",(object,),{"get_z_compensation_value" : get_z_compensation_value, "raw_axis_twist_comp" : self.raw_axis_twist_comp})
                 self.mod_axis_twist_comp = axis_twist_comp()
             if hasattr(manual_probe, "ProbeResult"):
-                self.raw_axis_twist_comp = self.mod_axis_twist_comp
                 def get_z_compensation_value(self, pos):
                     (x, y, z) = self.get_offsets()
                     temp = [manual_probe.ProbeResult(
@@ -750,7 +749,7 @@ class Scanner:
                         pos[2],
                     )]
                     bed_z = temp[0].bed_z
-                    self.raw_axis_twist_comp.get_z_compensation_value(temp)
+                    self.raw_axis_twist_comp._update_z_compensation_value(temp)
                     return temp[0].bed_z - bed_z
                 axis_twist_comp = type("class",(object,),{"get_z_compensation_value" : get_z_compensation_value, "get_offsets" : self.get_offsets, "raw_axis_twist_comp" : self.raw_axis_twist_comp})
                 self.mod_axis_twist_comp = axis_twist_comp()
